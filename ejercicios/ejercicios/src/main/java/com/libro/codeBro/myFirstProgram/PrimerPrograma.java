371487bddb4e854f9a72fa24ab2c958bd1ff9f3d
tomas <tomas.software@gmail.com>
2024-04-22 11:13:44 -0500

    voy a trabajar con el libro deitel

---
+++ ejercicios/ejercicios/src/main/java/com/libro/codeBro/myFirstProgram/PrimerPrograma.java
@@ -0,0 +1,10 @@
+package com.libro.codeBro.myFirstProgram;
+
+public class PrimerPrograma {
+    public static void main(String[] args) {
+        System.out.println("hola que tal !!");
+        System.out.println("Espero que esten bien !!");
+    }
+    
+}
+ 
