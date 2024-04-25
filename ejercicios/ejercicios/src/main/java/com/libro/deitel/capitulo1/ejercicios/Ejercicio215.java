83af8a04dc3c9092dc99559a66cb3d66bdf0983a
tomas <tomas.software@gmail.com>
2024-04-24 22:40:31 -0500

    ejercicios del miercoles, espero el jueves terminar capitulo 1

--- ejercicios/ejercicios/src/main/java/com/libro/deitel/capitulo1/ejercicios/Ejercicio215.java
+++ ejercicios/ejercicios/src/main/java/com/libro/deitel/capitulo1/ejercicios/Ejercicio215.java
@@ -1,10 +1,29 @@
 package com.libro.deitel.capitulo1.ejercicios;
 
+import java.util.Scanner;
+
 public class Ejercicio215 {
 
     public static void main(String[] args) {
 
-        // Estoy en el ejercicio numero 2.15 del libro de Deitel 
+        int a,b,suma,resta,producto,cociente;
+        @SuppressWarnings("resource")
+        Scanner sc = new Scanner(System.in);
+
+        // Estoy en el ejercicio numero 2.15 del libro de Deitel
+        System.out.println("Ingrese primer numero ");
+        a = sc.nextInt();
+        System.out.println("Ingrese el segundo numero");
+        b = sc.nextInt();
+        suma = a +b ;
+        System.out.println("La suma de los dos numeros es : " + suma);
+        producto = a * b ;
+        System.out.println("El producto de los dos numeros es : " + producto);
+        resta = a - b ;
+        System.out.println("La resta de los dos numeros es : " + resta);
+        cociente = a / b;
+        System.out.println("El cuociente de los dos numeros es :" + cociente);
+
 
         
     }
---
+++ ejercicios/ejercicios/src/main/java/com/libro/deitel/capitulo1/ejercicios/Ejercicio228.java
@@ -0,0 +1,7 @@
+package com.libro.deitel.capitulo1.ejercicios;
+
+public class Ejercicio228 {
+    public static void main(String[] args) {
+        
+    }
+}
