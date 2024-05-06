99b26f1098b951dadbf5b5178949724d1c0b1f32
tomas <tomas.software@gmail.com>
2024-05-06 02:46:15 -0500

    parte teoria de la leccion 3 terminada , ahora viene los ejercicios

---
+++ ejercicios/ejercicios/src/main/java/com/libro/deitel/capitulo3/Ejemplo1/Cuenta.java
@@ -0,0 +1,40 @@
+package com.libro.deitel.capitulo3.Ejemplo1;
+
+public class Cuenta {
+
+    private String nombre;// esto es una variable de instancia
+    //Ahora voy a crear lo que es un constructor
+    private double saldo;// esta variable la puedo usar en cualquier metodo
+    public Cuenta(String nombre, double saldo){
+        this.nombre = nombre; //osea esta instacia de atributo de la clase recibe una parametro de afuera 
+        // para poder inicializarlo
+        if (saldo > 0.0) { // Puedo colocar condiciones dentro de un constructor validaciones etc
+            this.saldo = saldo;
+        }
+        
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    //haciendo un metodo que deposito
+    public void depositar(double montoDeposito){
+        if (montoDeposito > 0.0) {
+            saldo = saldo + montoDeposito;
+            
+        }
+    }
+    public double getSaldo(){
+        return saldo;
+    }
+    public void setSaldo(double saldo){
+        this.saldo = saldo;
+    }
+    
+
+    
+}
---
+++ ejercicios/ejercicios/src/main/java/com/libro/deitel/capitulo3/Ejemplo1/PruebaCuenta.java
@@ -0,0 +1,28 @@
+package com.libro.deitel.capitulo3.Ejemplo1;
+
+import java.util.Scanner;
+
+public class PruebaCuenta {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String nombre;
+        double depo;
+        System.out.println("Ingrese el nombre");
+        Cuenta cu = new Cuenta("Nicola Gellona",123.98);//Ahora coloco el nombre por que se inicializa con el constructor
+        Cuenta cu1 = new Cuenta("Marco Antonio",34.23);
+        System.out.println("El nombre inicial es :"+cu.getNombre());
+        System.out.println(cu.getNombre() +" Tiene = " +cu.getSaldo());
+        System.out.println("El segundo nombre del objeto es :" +cu1.getNombre());
+        System.out.println(cu1.getNombre() +"Tiene = " +cu1.getSaldo());
+        System.out.println("Ingrese dinero a la cuenta de "+cu1.getNombre());
+        depo = sc.nextDouble();
+        cu1.depositar(depo);
+        System.out.println("Ingrese dinero a la cuenta de "+cu.getNombre());
+        depo = sc.nextDouble();
+        cu.depositar(depo);
+        System.out.println(cu1.getNombre() +" Ahora tiene "+cu1.getSaldo());
+        System.out.println(cu.getNombre() +" Ahora tiene "+cu.getSaldo());
+        
+    }
+    
+}
